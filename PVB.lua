2b45a6e22896101bdb7ae913f0535099de1ee311
HK-Scripttss <harukikun57@gmail.com>
2025-10-04 22:05:31 +0800

    Update PVB.lua

--- PVB.lua
+++ PVB.lua
@@ -1,2 +1,1 @@
-ID="8167f9c1-531d-4a0b-99ac-8dfbea5113af";
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HK-Scripttss/PlantsVsBrainrots/main/replicator", true))()
